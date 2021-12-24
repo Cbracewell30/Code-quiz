d1ab8b6821041fb13c357fe1e18c7a56e3f03f44
Chris <chrisbracewell35@gmail.com>
2021-12-23 23:07:50 -0500

    adding java for questions

--- script.js
+++ script.js
@@ -1,11 +1,48 @@
 var showResponse = document.getElementById('startbtn');
 var timerHTML = document.getElementById('timer');
 var scoreHTML = document.getElementById('score');
-var questionHTML = document.getElementById('quiz-container');
+var questionHTML = document.getElementById('quiz-questions');
 var button1HTML = document.getElementById('btn-1');
 var button2HTML = document.getElementById('btn-2');
 var button3HTML = document.getElementById('btn-3');
 var button4HTML = document.getElementById('btn-4');
+var thiscontainerHTML = document.getElementById("quiz-container")
+thiscontainerHTML.style.display = "none";
+
+var correctwrongHTML = document.getElementById('correct-wrong');
+
+button1HTML.addEventListener("click", showresult);
+button2HTML.addEventListener("click", showresult);
+button3HTML.addEventListener("click", showresult);
+button4HTML.addEventListener("click", showresult);
+
+var score = 0
+
+function showresult(){
+    var useranswer = this.getAttribute('data-value');
+    console.log(useranswer);
+
+if( useranswer == questionBank[questionList].answer){
+    correctwrongHTML.textContent = "Correct"
+    score += 5 
+}
+else{
+    correctwrongHTML.textContent = "Wrong"
+    counter -= 5
+}
+
+if (questionList < questionBank.length - 1 ){
+    questionList ++
+    quizstart ()
+}
+else {
+    alert('End of Quiz!')
+}
+
+};
+
+
+
 
 var questionBank = [
     {
@@ -36,7 +73,7 @@ var timeObj;
 var counter = 90
 
 showResponse.addEventListener("click",  function() {
-    document.getElementById("quiz-container").style.display = "block";
+    thiscontainerHTML.style.display = "block";
     timeObj = setInterval(() => {
       timerHTML.textContent = counter;
       if (counter > 1 ){
@@ -52,7 +89,7 @@ showResponse.addEventListener("click",  function() {
 });
 
 function quizstart (){
-    questionHTML.textContent = questionBank[questionList].question 
+    questionHTML.textContent = questionBank[questionList].question
     button1HTML.textContent = questionBank[questionList].choices[0]
     button2HTML.textContent = questionBank[questionList].choices[1]
     button3HTML.textContent = questionBank[questionList].choices[2]
